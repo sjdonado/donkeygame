4abd796893283dc5fd3fc62c192992c07d644b7a
Juan Rodriguez <sjdonado@uninorte.edu.co>
2017-10-26 13:42:41 -0500

    Se crea el objeto states y la funcion change_states

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,54 @@
 var game = new Phaser.Game((screen.availWidth - screen.availHeight*0.3)/2 , screen.availHeight - screen.availHeight*0.15, Phaser.CANVAS, '');
 
-var start_state = {
-	preload: function() {
-		game.stage.backgroundColor="#e52325";
-	},
+var controllers;
 
-	create: function() {	
-	},
+var states = {
+    start: {
+        preload: function() {
+            game.stage.backgroundColor="#e52325";
+        },
 
-	update: function() {
-	}
-};
+        create: function() {
+            controllers = game.input.keyboard.createCursorKeys();
+        },
 
-game.state.add('start', start_state);
+        update: function() {
+        }
+    },
+    main:{
+        preload: function() {
+            game.stage.backgroundColor="#ffff";
+        },
+
+        create: function() {    
+        },
+
+        update: function() {
+        }
+    },
+    finish:{
+        preload: function() {
+            game.stage.backgroundColor="#fafafa";
+        },
+
+        create: function() {    
+        },
+
+        update: function() {
+        }
+    }
+}
+
+function change_state(start_state, pause_state){
+    game.state.pause(pause_state);
+    game.state.resume(start_state);
+}
+
+game.state.add('start', states['start']);
+game.state.add('main', states['main']);
+game.state.add('finish', states['finish']);
 game.state.start('start');
+game.state.start('main');
+game.state.start('finish');
+game.state.pause('main');
+game.state.pause('finish');
