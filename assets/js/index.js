9ae0e078861f7e4a95170d802830f68735b50700
Juan Rodriguez <sjdonado@uninorte.edu.co>
2017-10-26 13:02:56 -0500

    States

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,16 @@
-var game = new Phaser.Game((screen.availWidth - screen.availHeight*0.3)/2 , screen.availHeight - screen.availHeight*0.15, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game((screen.availWidth - screen.availHeight*0.3)/2 , screen.availHeight - screen.availHeight*0.15, Phaser.CANVAS, '');
 
-function preload() {
-}
+var start_state = {
+	preload: function() {
+		game.stage.backgroundColor="#e52325";
+	},
 
-function create() {
-}
+	create: function() {	
+	},
 
-function update() {
-}
+	update: function() {
+	}
+};
+
+game.state.add('start', start_state);
+game.state.start('start');
