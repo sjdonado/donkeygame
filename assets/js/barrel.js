da2e0febe1d52493eaea9a0b1daa6095f952ebfe
Juan Rodriguez <sjdonado@uninorte.edu.co>
2017-10-28 15:01:18 -0500

    Quite el movimiento de donkey y lo centre

--- assets/js/barrel.js
+++ assets/js/barrel.js
@@ -5,19 +5,18 @@ var barrel = {
 		url: 'assets/sprites/barrel.png'
 	},
 	init: () =>{
-		sprite = this.game.add.sprite(110,110, 'barrel');
+		sprite = this.game.add.sprite(280,100, 'barrel');
 		sprite.visible = false;
 		barriles = this.game.add.group();
 		barriles.enableBody = true;
 	},
 	addBarrel: (value, position) => {
-		barrelObject = barriles.create(110, 110, 'barrel');
+		barrelObject = barriles.create(280, 100, 'barrel');
 		barrelObject.collideWorldBounds = true;
 		barrelObject.body.gravity.y = 300;
 		barrelObject.body.bounce.y = 0.3;
 		barrelObject.animations.add('animation', [0,1,4,3]);
 		barrelObject.animations.play('animation', 2, true);
-		barrelObject.body.x = position;
 		if(value == 1){
 			barrelObject.body.velocity.x = -100;
 		}else{
