2fed1acb1dc035bfb73cb2aaf31281e30e86da15
Juan Rodriguez <sjdonado@uninorte.edu.co>
2017-11-09 18:52:32 -0500

    Deploy heroku, ultimos test

--- client/assets/js/client.js
+++ client/assets/js/client.js
@@ -1,5 +1,5 @@
 var client = {
-	socket: io.connect('http://localhost:8080', {'forceNew': true}),
+	socket: io.connect('https://gamephaser.herokuapp.com', {'forceNew': true}),
 	arrayPlayers: [],
 	id: null,
 	askNewPlayer: ()=>{
